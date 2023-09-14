7f3806040147c566052aec7f7711f043ec4c3c72
Erick384 <erickwachuma@gmail.com>
2023-09-14 17:54:22 +0300

    Uploading Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import "./App.css";
-import markwebber from "../public/avatar-mark-webber.webp"
-import angela from "../public/avatar-angela-gray.webp"
-import jacob from "../public/avatar-jacob-thompson.webp"
-import rizky from "../public/avatar-rizky-hasanuddin.webp"
-import kimberly from "../public/avatar-kimberly-smith.webp"
-import nathan from "../public/avatar-nathan-peterson.webp"
-import anna from "../public/avatar-anna-kim.webp"
+import markwebber from "../public/avatar-mark-webber.webp";
+import angela from "../public/avatar-angela-gray.webp";
+import jacob from "../public/avatar-jacob-thompson.webp";
+import rizky from "../public/avatar-rizky-hasanuddin.webp";
+import kimberly from "../public/avatar-kimberly-smith.webp";
+import nathan from "../public/avatar-nathan-peterson.webp";
+import anna from "../public/avatar-anna-kim.webp";
+import myPic from "../public/image-chess.webp";
 
 function App() {
   return (
-    <>
+    <body>
       <div className="top-bar">
         <div className="title">
           <h2 className="title-text">Notifications</h2>
-          <h2 className="num" id="num">0</h2>
+          <h3 className="num" id="num">
+            0
+          </h3>
         </div>
         <a href="#" className="read" id="read">
           Mark all as read
@@ -22,7 +25,7 @@ function App() {
       <div className="notifications">
         {/* single notification starts */}
 
-        <div className="single-box">
+        <div className="single-box unread">
           <div className="avatar">
             <img src={markwebber} alt="Mark-Webber" />
           </div>
@@ -30,10 +33,12 @@ function App() {
             <p className="notifi-text">
               <a href="#" className="profile-name">
                 Mark Webber
-              </a> reacted to your recent post
+              </a>{" "}
+              reacted to your recent post{" "}
               <a href="#" className="post">
                 My First tournament today!
               </a>
+              <span className="dot"></span>
             </p>
             <p className="time">1m ago</p>
           </div>
@@ -51,8 +56,9 @@ function App() {
             <p className="notifi-text">
               <a href="#" className="profile-name">
                 Angela Gray
-              </a>
+              </a>{" "}
               followed you
+              <span className="dot"></span>
             </p>
             <p className="time">5m ago</p>
           </div>
@@ -70,11 +76,12 @@ function App() {
             <p className="notifi-text">
               <a href="#" className="profile-name">
                 Jacob Thompson
-              </a>
-              has joined your group
+              </a>{" "}
+              has joined your group{" "}
               <a href="#" className="post">
                 Chess Club
               </a>
+              <span className="dot"></span>
             </p>
             <p className="time">1 day ago</p>
           </div>
@@ -92,17 +99,16 @@ function App() {
             <p className="notifi-text">
               <a href="#" className="profile-name">
                 Rizky Hasanuddin
-              </a>
+              </a>{" "}
               sent you a private message
-              <a href="#" className="post">
-                
-                
-              </a>
+              <span className="dot"></span>
             </p>
             <p className="time">5 days ago</p>
-            <div className="private-text">Hello, thanks for setting up the Chess Club. I've been a member
-                for a few weeks now and I'm already having lots of fun and
-                improving my game.</div>
+            <div className="private-text">
+              Hello, thanks for setting up the Chess Club. I've been a member
+              for a few weeks now and I'm already having lots of fun and
+              improving my game.
+            </div>
           </div>
         </div>
 
@@ -110,7 +116,7 @@ function App() {
 
         {/* single notification starts */}
 
-        <div className="single-box">
+        <div className="single-box ">
           <div className="avatar">
             <img src={kimberly} alt="Kimberly-Smith" />
           </div>
@@ -118,11 +124,15 @@ function App() {
             <p className="notifi-text">
               <a href="#" className="profile-name">
                 Kimberly Smith
-              </a>
+              </a>{" "}
               commented on your picture
-              <a href="#" className="post"></a>
+              <span className="dot"></span>
             </p>
             <p className="time">1 week ago</p>
+          </div>
+
+          <div className="left-img">
+            <img src={myPic} alt="myPic" />
           </div>
         </div>
 
@@ -138,11 +148,12 @@ function App() {
             <p className="notifi-text">
               <a href="#" className="profile-name">
                 Nathan Peterson
-              </a>
-              reacted to your recent post
+              </a>{" "}
+              reacted to your recent post{" "}
               <a href="#" className="post">
                 5 end-game strategies to increase your win rate
               </a>
+              <span className="dot"></span>
             </p>
             <p className="time">2 weeks ago</p>
           </div>
@@ -159,12 +170,13 @@ function App() {
           <div className="box-text">
             <p className="notifi-text">
               <a href="#" className="profile-name">
-                Anna Kim
+                Anna Kim{" "}
               </a>
-              left the group
+              left the group{" "}
               <a href="#" className="post">
                 Chess Club
               </a>
+              <span className="dot"></span>
             </p>
             <p className="time">2 weeks ago</p>
           </div>
@@ -173,7 +185,6 @@ function App() {
         {/* Single notification end */}
       </div>
 
-
       <div className="attribution">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
@@ -181,7 +192,7 @@ function App() {
         </a>
         . Coded by <a href="https://github.com/Erick384">Erick-384</a>.
       </div>
-    </>
+    </body>
   );
 }
 
